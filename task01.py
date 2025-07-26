637caf999382f943773f72e925f28e15aa50b60a
Kento-Nanami28 <aneehc87@gmail.com>
2025-07-26 06:51:18 -0700

    Add files via upload

---
+++ task01.py
@@ -0,0 +1,20 @@
+
+number1 =input('number1:')
+number2 =input('number2:')
+sum = float(number1) + float(number2)
+print(sum)
+
+number1 =input('number1:')
+number2 =input('number2:')
+sum = float(number1) - float(number2)
+print(sum)
+
+number1 =input('number1:')
+number2 =input('number2:')
+sum = float(number1) * float(number2)
+print(sum)
+
+number1 =input('number1:')
+number2 =input('number2:')
+sum = float(number1) / float(number2)
+print(sum)
---
+++ task02.py
@@ -0,0 +1,7 @@
+firstname= input('firstname:')
+lastname= input('lastname:')
+
+print("Hello," + firstname +  lastname + ' How are you?')
+
+
+
