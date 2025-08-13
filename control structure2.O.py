4b9212a78f2fa43a5f9e8efb31752acde2203ca0
Kento-Nanami28 <aneehc87@gmail.com>
2025-08-12 21:35:14 -0700

    Add files via upload

---
+++ control structure2.O.py
@@ -0,0 +1,21 @@
+
+'''
+
+count= 1
+while count< 51 :
+    print(count)
+    count = count + 1
+
+    '''
+
+number = int(input("Enter a number: "))
+n=int(number)
+total_sum = 0
+print("calculate sum from 1 to 'n'")
+for i in range(1,n+1):
+    total_sum = total_sum + i
+    print(total_sum)
+
+
+
+
